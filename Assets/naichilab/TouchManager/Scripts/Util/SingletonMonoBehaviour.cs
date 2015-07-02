4e9649f842ba39601d0dd96b7e5df99ba4f26bad
Es_Program <radwimps.september@gmail.com>
2015-07-02 18:25:47 +0900

    make Utility folder and create script to generalization the Singleton pattern.

---
+++ Assets/naichilab/TouchManager/Scripts/Util/SingletonMonoBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class SingletonMonoBehaviour<T> : MonoBehaviour where T : MonoBehaviour
+{
+  private static T instance;
+
+  public static T Instance
+  {
+    get
+    {
+      if(instance == null)
+      {
+        instance = FindObjectOfType<T>();
+
+        if(instance == null)
+        {
+          Debug.LogWarning(typeof(T) + "is nothing");
+        }
+      }
+      return instance;
+    }
+  }
+}
