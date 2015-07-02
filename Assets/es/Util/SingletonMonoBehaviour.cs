748dedb70cbde758d01afd29648952f07b58a789
Es_Program <radwimps.september@gmail.com>
2015-07-02 19:11:36 +0900

    Changed the folder hierarchy and fixed indent.

---
+++ Assets/es/Util/SingletonMonoBehaviour.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class SingletonMonoBehaviour<T> : MonoBehaviour where T : MonoBehaviour
+{
+  private static T instance;
+
+  public static T Instance
+  {
+    get
+    {
+      if(instance == null)
+      {
+        instance = FindObjectOfType<T>();
+
+        if(instance == null)
+        {
+          Debug.LogWarning(typeof(T) + "is nothing");
+        }
+      }
+      return instance;
+    }
+  }
+
+  private void Awake()
+  {
+    if(this != Instance)
+    {
+      Destroy(this);
+      return;
+    }
+    DontDestroyOnLoad(this.gameObject);
+  }
+
+}
