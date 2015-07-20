13f2f407a7c85d11b50c7be65cd3a93b9669efc0
Es_Program <radwimps.september@gmail.com>
2015-07-21 01:35:12 +0900

    TouchListenerの追加
    TouchListenerでは以下の要素を追加
    ・押下時にレイを飛ばしてオブジェクトを取得
    ・レイがヒットした位置を取得可能
    ・クリックイベントが追加可能
    
    今後のタスク
    ・クリックイベントだけでなく、ドラッグ操作等も追加していく
    ・取得できる情報の追加

---
+++ Assets/es/TouchListener.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using UnityEngine;
+
+/// <summary>
+/// タッチ入力に応じた処理のイベントを提供する
+/// </summary>
+public class TouchListener : SingletonMonoBehaviour<TouchListener>
+{
+  /// <summary>
+  /// オブジェクトクリック時のイベント処理
+  /// </summary>
+  public event EventHandler<CustomInputEventArgs> ClickEvent;
+
+  private GameObject touchObject;
+  private RaycastHit hit;
+
+  /// <summary>
+  /// 現在タッチしているオブジェクト
+  /// </summary>
+  public GameObject TouchObject { get { return touchObject; } }
+
+  /// <summary>
+  /// タッチによってレイが衝突した最後のポジション
+  /// </summary>
+  public Vector3 HitPoint
+  {
+    get
+    {
+      return hit.point;
+    }
+  }
+
+  public void Awake()
+  {
+    TouchManager.Instance.TouchStart += OnTouchStart;
+    TouchManager.Instance.TouchEnd += OnTouchEnd;
+  }
+
+  /// <summary>
+  /// タッチ開始時の動作
+  /// </summary>
+  private void OnTouchStart(object sender, CustomInputEventArgs args)
+  {
+    //最初にヒットしたオブジェクトを取得する
+    if(Physics.Raycast(Camera.main.ScreenPointToRay(args.Input.ScreenPosition), out hit))
+      touchObject = hit.collider.gameObject;
+    
+  }
+
+  /// <summary>
+  /// リリース時のイベント
+  /// </summary>
+  private void OnTouchEnd(object sender, CustomInputEventArgs args)
+  {
+    RaycastHit hit;
+    if(Physics.Raycast(Camera.main.ScreenPointToRay(args.Input.ScreenPosition), out hit))
+      if(GameObject.ReferenceEquals(hit.collider.gameObject, touchObject))
+        if(ClickEvent != null)
+          ClickEvent(sender, args);
+  }
+}
